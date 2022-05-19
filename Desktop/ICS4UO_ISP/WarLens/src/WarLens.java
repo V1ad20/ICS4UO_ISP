5d15ccb31c5e9910841b52a6af2003966f2ddfd2
Sean <seany071305@gmail.com>
2022-05-19 14:54:47 -0400

    set warlens to sample circle scale thing (experimental)

--- Desktop/ICS4UO_ISP/WarLens/src/WarLens.java
+++ Desktop/ICS4UO_ISP/WarLens/src/WarLens.java
@@ -5,30 +5,38 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.animation.*;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
  
 public class WarLens extends Application {
-    @FXML
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage splashStage) {
+        Circle circle = new Circle();
+        circle.setCenterX(150.0);
+        circle.setCenterY(150.0);
+        circle.setRadius(50.0);
+        circle.setFill(Color.BROWN);
+        circle.setStrokeWidth(10);
+
+        ScaleTransition scale = new ScaleTransition();
+        scale.setDuration(Duration.millis(1000.0));
+        scale.setNode(circle);
+        scale.setByX(2.0);
+        scale.setByY(2.0);
+        scale.setCycleCount(50);
+        scale.setAutoReverse(true);
+        scale.play();
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(circle);
 
- Scene scene = new Scene(root, 300, 250);
+ Scene scene = new Scene(root, 300, 300);
 
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        splashStage.setTitle("Test Splash");
+        splashStage.setScene(scene);
+        splashStage.show();
     }
  public static void main(String[] args) {
         launch(args);
