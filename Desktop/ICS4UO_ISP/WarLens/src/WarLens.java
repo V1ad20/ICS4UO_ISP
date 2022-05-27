52c27df120dfc118fb347ee6392c85bb0f767ec0
Sean <seany071305@gmail.com>
2022-05-27 12:48:22 -0400

    deleted unused libraries

--- Desktop/ICS4UO_ISP/WarLens/src/WarLens.java
+++ Desktop/ICS4UO_ISP/WarLens/src/WarLens.java
@@ -1,35 +1,18 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import java.awt.image.BufferedImage;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.imageio.spi.ImageWriterSpi;
-
-import javafx.animation.*;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
 
 public class WarLens extends Application {
     PicturePixel[][] pictureArr;
 
     public void start(Stage splashStage) throws IOException{
 
-        loadArr(10, 10);
+        loadArr(30, 30);
 
         Group root = new Group();
         
@@ -62,8 +45,8 @@ public class WarLens extends Application {
         for (int row = 0; row < pictureArr.length; row++){
             for (int col = 0; col < pictureArr[0].length; col++){
                 PicturePixel p = pictureArr[row][col];
-                p.imageView.setX(x+col*(p.sideLength+10));
-                p.imageView.setY(y+row*(p.sideLength+10));
+                p.imageView.setX(x+col*(p.sideLength+1));
+                p.imageView.setY(y+row*(p.sideLength+1));
                 System.out.println(x+col*p.sideLength);
                 System.out.println(y+row*p.sideLength); 
                 System.out.println(p);
