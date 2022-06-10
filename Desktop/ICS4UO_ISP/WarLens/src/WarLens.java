643fa738612459dfe4933bd910f0eb6e2781da8c
Sean <seany071305@gmail.com>
2022-06-09 20:25:34 -0400

    edit

--- Desktop/ICS4UO_ISP/WarLens/src/WarLens.java
+++ Desktop/ICS4UO_ISP/WarLens/src/WarLens.java
@@ -7,6 +7,7 @@
  * @since 2022-05-19
  */
 
+import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
@@ -16,7 +17,6 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
@@ -134,16 +134,47 @@ public class WarLens extends Application {
         Group root = new Group();
         Scene scene2 = new Scene(root, 640, 640);
 
-        Image character = new Image("resources/characters/mainCharacter/test.gif");
-        ImageView testChar = new ImageView(character);
+        Image frame1 = new Image("resources/characters/mainCharacter/frame1.png");
+        Image frame2 = new Image("resources/characters/mainCharacter/frame2.png");
+        Image frame3 = new Image("resources/characters/mainCharacter/frame3.png");
+        Image frame4 = new Image("resources/characters/mainCharacter/frame4.png");
+
+        ImageView testChar = new ImageView();
 
         testChar.setX(-10);
         testChar.setY(400);
         testChar.setPreserveRatio(true);
         testChar.setScaleX(1);
         testChar.setScaleY(1);
-        
+
         root.getChildren().add(testChar);
+
+        ArrayList<Image> rightMoveFrames = new ArrayList<Image>();
+        rightMoveFrames.add(frame1);
+        rightMoveFrames.add(frame2);
+        rightMoveFrames.add(frame3);
+        rightMoveFrames.add(frame4);
+
+        checkTime = 0;
+        curTime = 0;
+        curIndex = 0;
+
+        AnimationTimer runningRightAnim = new AnimationTimer() {
+            @Override
+            public void handle(long nanos) {
+                int milis = (int) (nanos / 1000000);
+                curTime = (int) Math.floor(0.004 * milis);
+                if (checkTime != curTime) {
+                    testChar.setImage(rightMoveFrames.get(curIndex));
+                    curIndex++;
+                    if(curIndex == rightMoveFrames.size()){
+                        curIndex = 0;
+                    }
+                    checkTime = curTime;
+                }
+            }
+        };
+        runningRightAnim.start();
 
         TranslateTransition charMove = new TranslateTransition();
         charMove.setDuration(Duration.millis(5000));
@@ -153,9 +184,8 @@ public class WarLens extends Application {
         charMove.setCycleCount(1);
         charMove.setAutoReverse(false);
         charMove.play();
-        
 
-        //textTool("Desktop/ICS4UO_ISP/WarLens/src/scene2Text.txt", root, scene2);
+        textTool("resources/scene2Text.txt", root, scene2);
 
         stage.setScene(scene2);
         stage.show();
@@ -165,7 +195,7 @@ public class WarLens extends Application {
 
         ArrayList<String> textCache = new ArrayList<String>();
 
-        Scanner sc = new Scanner(new File(filepath));
+        Scanner sc = new Scanner(new File("/scene2Text.txt"));
 
         while (sc.hasNext()) {
             textCache.add(sc.nextLine());
@@ -174,7 +204,7 @@ public class WarLens extends Application {
 
         currentString = textCache.get(curIndex);
 
-        Image textBoxImage = new Image("textBox.png");
+        Image textBoxImage = new Image("resources/textBox.png");
         ImageView textBox = new ImageView(textBoxImage);
         textBox.setX(0);
         textBox.setY(506);
@@ -189,7 +219,7 @@ public class WarLens extends Application {
         text.setFill(Color.WHITE);
         root.getChildren().add(text);
 
-        Image arrowImage = new Image("arrow.png");
+        Image arrowImage = new Image("resources/arrow.png");
         ImageView arrow = new ImageView(arrowImage);
         arrow.setX(590);
         arrow.setY(605);
@@ -283,81 +313,6 @@ public class WarLens extends Application {
     }
 
     /**
-     * This method loads a 2D array of size b and h with the
-     * "redPixel.png" image
-     * Worked on by: Vlad, Sean
-     * 
-     * @param b number of columns
-     * @param h number of rows
-     */
-    public void loadArr(int b, int h) {
-        pictureArr = new PicturePixel[h][b];
-        for (int row = 0; row < h; row++) {
-            for (int col = 0; col < b; col++) {
-                Image img = new Image("/redPixel.png");
-                pictureArr[row][col] = new PicturePixel(img, 1, img.getHeight());
-            }
-        }
-    }
-
-    /**
-     * This method displays the array by adding the images to
-     * the root stack starting at the x and y coordinates
-     * Worked on by: Vlad
-     * 
-     * @param x    x coordinate of where to start adding pictures
-     * @param y    y coordinate of where to start adding pictures
-     * @param root root stack
-     */
-    public void displayArr(int x, int y, Group root) {
-        for (int row = 0; row < pictureArr.length; row++) {
-            for (int col = 0; col < pictureArr[0].length; col++) {
-                PicturePixel p = pictureArr[row][col];
-                p.imageView.setX(x + col * (p.sideLength + 2));
-                p.imageView.setY(y + row * (p.sideLength + 2));
-                root.getChildren().add(p.imageView);
-            }
-        }
-
-    }
-
-    /**
-     * This method is an experimental method to test two things for the stress
-     * mechanic
-     * of the game. Test 1: blur effect Test 2: transparent PNG images
-     * Worked on by: Sean
-     * 
-     * @param strength sets the radius (i.e. strength) of the blur effect
-     * @param root     root stack
-     * @throws IOException on image not found
-     */
-    public void stressEffects(int strength, Group root) throws IOException {
-        Image image = new Image("/test.png"); // use test image for now change it later
-        ImageView iv = new ImageView(image);
-        iv.setX(0.0);
-        iv.setY(0.0);
-        iv.setFitWidth(300.0);
-        iv.setFitHeight(300.0);
-        iv.setPreserveRatio(true);
-
-        GaussianBlur gb = new GaussianBlur();
-        gb.setRadius(strength); // due to nature of gaussian blur, the radius controls the strength of the blur
-        iv.setEffect(gb);
-        root.getChildren().add(iv);
-
-        Image vignette = new Image("/vignette.png"); // testing vignette image. Also testing for transparent backgrounds
-                                                     // with PNG. Looks like it works?
-        ImageView vin = new ImageView(vignette);
-        vin.setX(0.0);
-        vin.setY(0.0);
-        vin.setFitWidth(300.0);
-        vin.setFitHeight(300.0);
-        vin.setPreserveRatio(false);
-        root.getChildren().add(vin);
-        // for vignetting maybe also blur it? maybe not
-    }
-
-    /**
      * Main Method that launches the application
      * 
      * @param args
@@ -365,33 +320,4 @@ public class WarLens extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-    // /**
-    // * This method is an experimental method for collision checking
-    // * based on nearby colours
-    // * Worked on by: Sean
-    // *
-    // * @param x x coordinate of where to check
-    // * @param y y coordinate of where to check
-    // * @return boolean returns true if the player can move and false if they
-    // cannot
-    // * (i.e. colour not detected, colour detected)
-    // * @throws IOException on image not found
-    // */
-    // public boolean collisionCheck(int x, int y) throws IOException {
-    // BufferedImage image = ImageIO.read(getClass().getResource("/test.png")); //
-    // we need to use bufferedImage here
-    // // since getRGB only works with
-    // // bufferedImage
-
-    // int check = image.getRGB(x, y); // getRGB returns a really weird integer,
-    // have to fix this later
-    // System.out.println(check);
-    // if (check == 000000000) { // fix this with getRGB if we're going to use
-    // colour picking
-    // return false;
-    // } else {
-    // return true;
-    // }
-    // }
 }
